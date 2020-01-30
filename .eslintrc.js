349eb8a79a6a68efe795ea42d5530cc752b3e9a0
ngirimana <chadrackngirimana@gmail.com>
2020-01-30 17:27:27 +0200

    remove   es5

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,16 @@
 module.exports = {
-  extends: ["airbnb-base"],
-  parser: "babel-eslint",
-  env: {
-    mocha: true
-  },
-  rules: {
-    "linebreak-style": [
-      "error",
-      process.env.NODE_ENV === "prod" ? "unix" : "windows"
-    ],
-    "no-restricted-globals": "off",
-    "consistent-return": "off"
-  }
+    extends: ["airbnb-base"],
+    parser: "babel-eslint",
+    env: {
+        mocha: true
+    },
+    rules: {
+        "linebreak-style": [
+            "error",
+            process.env.NODE_ENV === "prod" ? "unix" : "windows"
+        ],
+        "no-restricted-globals": "off",
+        "consistent-return": "off"
+        "import/prefer-default-export": "import/prefer-default-export"
+    }
 };
--- server/controllers/userConntroller.js
+++ server/controllers/userConntroller.js
@@ -1,31 +1,31 @@
-const User = require('./../models/userModels');
+const User = require("./../models/userModels");
 
-exports.createuser = async (req, res) => {
-  try {
-    const newUser = await User.create(req.body);
-    res.status(201).json({
-      status: 'User succefull created',
-      data: { user: newUser },
-    });
-  } catch (error) {
-    res.status(400).json({
-      status: 'fail',
-      message: error,
-    });
-  }
+export const createuser = async(req, res) => {
+    try {
+        const newUser = await User.create(req.body);
+        res.status(201).json({
+            status: "User succefull created",
+            data: { user: newUser }
+        });
+    } catch (error) {
+        res.status(400).json({
+            status: "fail",
+            message: error
+        });
+    }
 };
-exports.signinuser = async (req, res) => {
-  try {
-    const userlogin = await User.findOne(req.body);
+export const signinuser = async(req, res) => {
+    try {
+        const userlogin = await User.findOne(req.body);
 
-    res.status(202).json({
-      status: 'User is  Availabel',
-      data: { user: userlogin },
-    });
-  } catch (error) {
-    res.status(400).json({
-      status: 'fail',
-      message: error,
-    });
-  }
+        res.status(202).json({
+            status: "User is  Availabel",
+            data: { user: userlogin }
+        });
+    } catch (error) {
+        res.status(400).json({
+            status: "fail",
+            message: error
+        });
+    }
 };
