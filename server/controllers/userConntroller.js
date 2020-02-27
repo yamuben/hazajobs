852cace05b6df14bf570f2a34bd6cf653cc4d892
yamuben <yamubbenjamin@gmail.com>
2020-02-27 11:15:36 +0200

    [

--- server/controllers/userConntroller.js
+++ server/controllers/userConntroller.js
@@ -1,111 +1,121 @@
-/* eslint-disable prefer-const */
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-console */
-/* eslint-disable eol-last */
-/* eslint-disable comma-dangle */
-/* eslint-disable function-paren-newline */
-/* eslint-disable quotes */
-/* eslint-disable no-underscore-dangle */
-/* eslint-disable indent */
-/* eslint-disable space-before-function-paren */
-/* eslint-disable linebreak-style */
-// eslint-disable-next-line linebreak-st
-// eslint-disable-next-line linebreak-st
-import lodash from "lodash";
-import dotenv from "dotenv";
-import { encryptPassword, decryptPassword } from "../helpers/securedPassword";
-import User from "../models/userModels";
 
-import response from "../helpers/responses";
+import lodash from 'lodash';
+import dotenv from 'dotenv';
+import { encryptPassword, decryptPassword } from '../helpers/securedPassword';
+import User from '../models/userModels';
 
-import { generateAuthToken } from "../helpers/tokens";
+import response from '../helpers/responses';
+
+import { generateAuthToken, userIdFromToken } from '../helpers/tokens';
+
 
 dotenv.config();
 
-const createuser = async(req, res) => {
-    try {
-        let {
-            userType,
-            firstName,
-            lastName,
-            phoneNumber,
-            email,
-            password
-        } = req.body;
-        password = encryptPassword(password);
-        const newUser = await User.create({
-            userType,
-            firstName,
-            lastName,
-            phoneNumber,
-            email,
-            password
-        });
-        const token = generateAuthToken(
-            newUser._id,
-            newUser.phoneNumber,
-            newUser.email
-        );
-        const data = {
-            token,
-            userData: lodash.pick(
-                newUser,
-                "id",
-                "userType",
-                "firstName",
-                "lastName",
-                "email"
-            )
-        };
-        return response.successResponse(
-            res,
+const createuser = async (req, res) => {
+  try {
+    let {
+      userType,
+      firstName,
+      lastName,
+      phoneNumber,
+      email,
+      password,
+    } = req.body;
+    password = encryptPassword(password);
+    const newUser = await User.create({
+      userType,
+      firstName,
+      lastName,
+      phoneNumber,
+      email,
+      password,
+    });
+    const token = generateAuthToken(
+      newUser._id,
+      newUser.phoneNumber,
+      newUser.email,
+    );
+    const data = {
+      token,
+      userData: lodash.pick(
+        newUser,
+        'id',
+        'userType',
+        'firstName',
+        'lastName',
+        'email',
+      ),
+    };
+    return response.successResponse(
+      res,
 
-            201,
+      201,
 
-            "User created successfully",
-            data
-        );
-    } catch (error) {
-        return response.errorResponse(res, 400, error);
-    }
+      'User created successfully',
+      data,
+    );
+  } catch (error) {
+    return response.errorResponse(res, 400, error);
+  }
 };
 
-const signinuser = async(req, res) => {
-    try {
-        const { email, password } = req.body;
-        const userLogin = await User.findOne({ email });
-        if (userLogin && decryptPassword(password, userLogin.password)) {
-            const token = generateAuthToken(
-                userLogin._id,
-                userLogin.phoneNumber,
-                userLogin.email
-            );
-            const data = {
-                token,
-                userData: lodash.pick(
-                    userLogin,
-                    "id",
-                    "userType",
-                    "firstName",
-                    "lastName",
-                    "email"
-                )
-            };
+const signinuser = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const userLogin = await User.findOne({ $or: [{ email: username }, { phoneNumber: username }] });
+    if (userLogin && decryptPassword(password, userLogin.password)) {
+      const token = generateAuthToken(
+        userLogin._id,
+        userLogin.phoneNumber,
+        userLogin.email,
+      );
+      const data = {
+        token,
+        userData: lodash.pick(
+          userLogin,
+          'id',
+          'userType',
+          'firstName',
+          'lastName',
+          'email',
+        ),
+      };
 
-            return response.successResponse(
-                res,
+      return response.successResponse(
+        res,
 
-                200,
+        200,
 
-                "User logged in successfully",
+        'User logged in successfully',
 
-                data
-            );
-        }
-        return response.errorResponse(res, 401, "Incorrect email or password");
-    } catch (error) {
-        return response.errorResponse(res, 500, error);
+        data,
+      );
     }
+    return response.errorResponse(res, 401, 'Incorrect email or password');
+  } catch (error) {
+    return response.errorResponse(res, 500, error);
+  }
 };
 
-export default { createuser, signinuser };
+
+const updateUserProfile = async (req, res) => {
+  const userId = userIdFromToken(req.header('x-auth-token'));
+  try {
+    // console(userId._id)
+    const profile = await User.findByIdAndUpdate(userId, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    return response.successResponse(
+      res,
+
+      200,
+
+      'User profile updated successfully',
+      profile,
+    );
+  } catch (error) {
+    return response.errorResponse(res, 400, error);
+  }
+};
+export default { createuser, signinuser, updateUserProfile };
--- server/models/userModels.js
+++ server/models/userModels.js
@@ -1,5 +1,5 @@
-import validator from "validator";
-import mongoose from "mongoose";
+import validator from 'validator';
+import mongoose from 'mongoose';
 
 // First name *
 //   Last name *
@@ -20,89 +20,89 @@ import mongoose from "mongoose";
 // password
 
 const userSchema = new mongoose.Schema({
-    userType: {
-        type: String,
-        // required: [true, 'choose  between  individual  or company'],
-        default: "individual",
-        enum: {
-            values: ["Individual", "Company"],
-            messages: "User type must be Individual / Company"
-        },
-        trim: true
+  userType: {
+    type: String,
+    // required: [true, 'choose  between  individual  or company'],
+    default: 'individual',
+    enum: {
+      values: ['Individual', 'Company'],
+      messages: 'User type must be Individual / Company',
     },
-    firstName: {
-        type: String,
-        required: [true, "please  tell us your  name"],
-        trim: true
-    },
-    lastName: {
-        type: String,
-        required: [true, "please  tell us your  name"],
-        trim: true
-    },
-    phoneNumber: {
-        type: String,
-        required: [true, "please  provide your  phone number"],
-        trim: true,
-        unique: true
-    },
-    email: {
-        type: String,
-        required: [true, "please  tell us your  email"],
+    trim: true,
+  },
+  firstName: {
+    type: String,
+    required: [true, 'please  tell us your  name'],
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: [true, 'please  tell us your  name'],
+    trim: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: [true, 'please  provide your  phone number'],
+    trim: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, 'please  tell us your  email'],
 
-        unique: true,
-        lowercase: true,
-        validate: [validator.isEmail, "Provide valid email"]
-    },
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Provide valid email'],
+  },
 
-    password: {
-        type: String,
-        required: [true, "please  provide your  password"]
-    },
-    gender: {
-        type: String,
-        default: "",
-        trim: true
-    },
-    id_passport: {
-        type: String,
-        default: "",
-        trim: true
-    },
-    profilepicture: {
-        type: String,
-        default: "",
-        trim: true
-    },
-    location: [String],
-    skills: [String],
-    selfdescription: {
-        type: String,
-        default: "",
-        trim: true
-    },
-    portifolio: {
-        type: String,
-        default: "",
-        trim: true
-    },
-    education: {
-        type: String,
-        default: "",
-        trim: true
-    },
-    experience: {
-        type: String,
-        default: "",
-        trim: true
-    },
-    certification: [String],
-    status: {
-        type: String,
-        default: "",
-        trim: true
-    }
+  password: {
+    type: String,
+    required: [true, 'please  provide your  password'],
+  },
+  gender: {
+    type: String,
+    default: '',
+    trim: true,
+  },
+  id_passport: {
+    type: String,
+    default: '',
+    trim: true,
+  },
+  profilepicture: {
+    type: String,
+    default: '',
+    trim: true,
+  },
+  location: [String],
+  skills: [String],
+  selfdescription: {
+    type: String,
+    default: '',
+    trim: true,
+  },
+  portifolio: {
+    type: String,
+    default: '',
+    trim: true,
+  },
+  education: {
+    type: String,
+    default: '',
+    trim: true,
+  },
+  experience: {
+    type: String,
+    default: '',
+    trim: true,
+  },
+  certification: [String],
+  status: {
+    type: String,
+    default: '',
+    trim: true,
+  },
 });
-const Userprofile = mongoose.model("Userprofile", userSchema);
+const Userprofile = mongoose.model('Userprofile', userSchema);
 
 export default Userprofile;
