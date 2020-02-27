e465e8c040735b805c8e4bb8c89d99fdea07b165
ngirimana <chadrackngirimana@gmail.com>
2020-02-27 16:42:02 +0200

    removing id, password,_v from response

--- server/controllers/userConntroller.js
+++ server/controllers/userConntroller.js
@@ -53,7 +53,7 @@ const createuser = async (req, res) => {
 const signinuser = async (req, res) => {
   try {
     const { username, password } = req.body;
-    const userLogin = await User.findOne({ $or: [{ email: username }, { phoneNumber: username }] });
+    const userLogin = await User.findOne({ $or: [ { email: username }, { phoneNumber: username } ] });
     if (userLogin && decryptPassword(password, userLogin.password)) {
       const token = generateAuthToken(
         userLogin._id,
@@ -88,8 +88,11 @@ const updateUserProfile = async (req, res) => {
       new: true,
       runValidators: true,
     });
+
+    const userProfile = await User.findById(profile._id,
+      { password: 0, __v: 0, _id: 0 });
     return response.successResponse(
-      res, 200, 'User profile updated successfully', profile,
+      res, 200, 'User profile updated successfully', userProfile,
     );
   } catch (error) {
     return response.errorResponse(res, 400, error);
@@ -98,7 +101,8 @@ const updateUserProfile = async (req, res) => {
 const viewProfile = async (req, res) => {
   try {
     const { searchId } = req.params;
-    const userProfile = await User.findById(searchId);
+    const userProfile = await User.findById(searchId,
+      { password: 0, __v: 0, _id: 0 });
     return response.successResponse(
       res, 200, 'User profile retrieved successfully', userProfile,
     );
