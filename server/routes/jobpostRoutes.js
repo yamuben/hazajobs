2a5d53d67bf325709d28562242046bf04bb3eecf
ngirimana <chadrackngirimana@gmail.com>
2020-03-01 19:46:35 +0200

    add get macthing jobs to your data

--- server/routes/jobpostRoutes.js
+++ server/routes/jobpostRoutes.js
@@ -1,12 +1,11 @@
-/* eslint-disable eol-last */
-/* eslint-disable quotes */
-/* eslint-disable linebreak-style */
-import express from "express";
-import jobpostController from "../controllers/jobpostController";
-import { verifyAuth } from "../middlewares/authVerification";
+
+import express from 'express';
+import jobpostController from '../controllers/jobpostController';
+import { verifyAuth } from '../middlewares/authVerification';
 
 const jobpostrouter = express.Router();
 
-jobpostrouter.post("/newjobpost", verifyAuth, jobpostController.createnewjob);
-jobpostrouter.get("/jobs", jobpostController.findALLjobs);
+jobpostrouter.post('/newjobpost', verifyAuth, jobpostController.createnewjob);
+jobpostrouter.get('/jobs', jobpostController.findALLjobs);
+jobpostrouter.get('/matchingJobs', jobpostController.matchingJobs);
 export default jobpostrouter;
