0d489ba8e22d5f6512cb578b71f013d4055b8ba5
ngirimana <chadrackngirimana@gmail.com>
2020-03-03 13:42:03 +0200

    add route to search api

--- server/routes/jobpostRoutes.js
+++ server/routes/jobpostRoutes.js
@@ -9,4 +9,5 @@ jobpostrouter.post('/newjobpost', verifyAuth, jobpostController.createnewjob);
 jobpostrouter.get('/jobs', jobpostController.findALLjobs);
 jobpostrouter.get('/matchingJobs', verifyAuth, jobpostController.matchingJobs);
 jobpostrouter.delete('/matchingJobs/:jobId', verifyAuth, jobpostController.deleteJob);
+jobpostrouter.get('/jobs/:searchParameter', jobpostController.searchInJobs);
 export default jobpostrouter;
