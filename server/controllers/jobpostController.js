7f588fa8ab9ece6a969594f0fb1f4110ea1fbfc8
ngirimana <chadrackngirimana@gmail.com>
2020-03-04 13:20:33 +0200

    sort matching jobs

--- server/controllers/jobpostController.js
+++ server/controllers/jobpostController.js
@@ -81,7 +81,11 @@ const matchingJobs = async (req, res) => {
       const userJobs = await jobPost.find({
         $or: [ { 'jobqualification.first': user.skills.first }, { 'jobqualification.second': user.skills.second }, { 'joblocation.province': user.location.province }, { 'joblocation.district': user.location.district }, { 'joblocation.center': user.location.center } ],
       });
-      if (userJobs.length) { return response.successResponse(res, 200, 'Jobs matching your data', userJobs); }
+      if (userJobs.length) {
+        const sortedUserJobs = userJobs.sort((a, b) => (new Date(b.jobcreatedat)).getTime()
+          - (new Date(a.jobcreatedat).getTime()));
+        return response.successResponse(res, 200, 'Jobs matching your data', sortedUserJobs);
+      }
       return response.errorResponse(res, 404, 'Jobs matches with your data are not available');
     }
     return response.errorResponse(res, 401, 'You are not a user');
@@ -109,12 +113,12 @@ const searchInJobs = async (req, res) => {
     const { searchParameter } = req.params;
     const searchResult = await jobPost.find({
       $or: [
-        { organization: { $regex: `.*${searchParameter}.*` } },
-        { 'joblocation.province': { $regex: `.*${searchParameter}.*` } },
-        { 'joblocation.district': { $regex: `.*${searchParameter}.*` } },
-        { 'joblocation.center': { $regex: `.*${searchParameter}.*` } },
-        { 'jobqualification.first': { $regex: `.*${searchParameter}.*` } },
-        { 'jobqualification.second': { $regex: `.*${searchParameter}.*` } },
+        { organization: { $regex: `.*${ searchParameter }.*` } },
+        { 'joblocation.province': { $regex: `.*${ searchParameter }.*` } },
+        { 'joblocation.district': { $regex: `.*${ searchParameter }.*` } },
+        { 'joblocation.center': { $regex: `.*${ searchParameter }.*` } },
+        { 'jobqualification.first': { $regex: `.*${ searchParameter }.*` } },
+        { 'jobqualification.second': { $regex: `.*${ searchParameter }.*` } },
       ],
     });
     if (searchInJobs.length === 0) {
