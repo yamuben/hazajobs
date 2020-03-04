127dbddbcd79e01da435ab5e37a4d9d8c2029e23
ngirimana <chadrackngirimana@gmail.com>
2020-03-04 21:59:17 +0200

    add route to reject application api

--- server/routes/jobAppRoute.js
+++ server/routes/jobAppRoute.js
@@ -6,4 +6,5 @@ const jobPostAppRouter = express.Router();
 jobPostAppRouter.post('/jobApplication/:jobId', verifyAuth, jobPostAppController.createJobApplication);
 jobPostAppRouter.get('/myJobApplications/', verifyAuth, jobPostAppController.myJobApplications);
 jobPostAppRouter.get('/JobApplications/:jobAppId/accept', verifyAuth, jobPostAppController.accecptAplication);
+jobPostAppRouter.get('/JobApplications/:jobAppId/reject', verifyAuth, jobPostAppController.rejectApplication);
 export default jobPostAppRouter;
