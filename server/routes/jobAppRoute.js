ae8b9b4f83555000797963e412d1f3c4809ed89e
ngirimana <chadrackngirimana@gmail.com>
2020-03-04 11:26:02 +0200

    update route

--- server/routes/jobAppRoute.js
+++ server/routes/jobAppRoute.js
@@ -3,5 +3,5 @@ import { verifyAuth } from '../middlewares/authVerification';
 import jobPostAppController from '../controllers/jobPostAppController';
 
 const jobPostAppRouter = express.Router();
-jobPostAppRouter.post('/jobApplication:jobId', verifyAuth, jobPostAppController.createJobApplication);
+jobPostAppRouter.post('/jobApplication/:jobId', verifyAuth, jobPostAppController.createJobApplication);
 export default jobPostAppRouter;
