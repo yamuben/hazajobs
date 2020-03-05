93cf33fbe00ec55b1aae98391fb35132c068e4b1
ngirimana <chadrackngirimana@gmail.com>
2020-03-05 10:56:24 +0200

    add route to application made to commpany api

--- server/routes/jobAppRoute.js
+++ server/routes/jobAppRoute.js
@@ -5,6 +5,7 @@ import jobPostAppController from '../controllers/jobPostAppController';
 const jobPostAppRouter = express.Router();
 jobPostAppRouter.post('/jobApplication/:jobId', verifyAuth, jobPostAppController.createJobApplication);
 jobPostAppRouter.get('/myJobApplications/', verifyAuth, jobPostAppController.myJobApplications);
+jobPostAppRouter.get('/myCompanyApplication/', verifyAuth, jobPostAppController.myJobApplications);
 jobPostAppRouter.get('/JobApplications/:jobAppId/accept', verifyAuth, jobPostAppController.accecptAplication);
 jobPostAppRouter.get('/JobApplications/:jobAppId/reject', verifyAuth, jobPostAppController.rejectApplication);
 export default jobPostAppRouter;
